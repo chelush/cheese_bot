9a3f9f91c0f67711246895f8ffb92ad6f8c7e652
Maxim Kirpicev <makirpichev@edu.hse.ru>
2025-06-03 01:58:49 +0300

    Initial commit

--- bot/controllers/controllers.py
+++ bot/controllers/controllers.py
@@ -114,11 +114,11 @@ async def fetch_user(user_telegram: aiogram.types.User, session: AsyncSession = 
         query = select(User).where(User.telegram_id == user_telegram.id)
         result = await session.execute(query)
         user = result.scalar_one_or_none()
-        user.updated_at = datetime.datetime.now()
-        await session.flush()
         logger.info(f"Updated user {user.telegram_id=} at {user.updated_at}")
         if user is None:
             user = await create_user(user_telegram, session=session)
+        user.updated_at = datetime.datetime.now()
+        await session.flush()
         return user
 
 
