96962de5a9302debfce4fb7e36bef7657f720391
Maxim Kirpicev <makirpichev@edu.hse.ru>
2025-06-03 12:36:04 +0300

    Initial commit

--- bot/jobs/__init__.py
+++ bot/jobs/__init__.py
@@ -3,8 +3,5 @@ from . import send_notifications
 
 
 def setup_jobs():
-    # scheduler.add_job(send_notifications.send_notification_sale, "interval", hours=1)
-    # scheduler.add_job(send_notifications.send_notification_group, "interval", minutes=30)
-
-    scheduler.add_job(send_notifications.send_notification_sale, "interval", minutes=1)
-    scheduler.add_job(send_notifications.send_notification_group, "interval", minutes=2)
+    scheduler.add_job(send_notifications.send_notification_sale, "interval", hours=1)
+    scheduler.add_job(send_notifications.send_notification_group, "interval", minutes=45)
